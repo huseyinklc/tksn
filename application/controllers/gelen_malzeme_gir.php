fbb1f3b639e3ed74b042dd7d58a4d5da7ad200b4
huseyiklc <huseyinklc@yahoo.co.uk>
2013-08-12 10:43:26 +0300

    malzeme giriş formu oluşturuldu, bu form validation yapıldı ve başarılı sayfası oluşturuldu

--- application/controllers/gelen_malzeme_gir.php
+++ application/controllers/gelen_malzeme_gir.php
@@ -6,8 +6,20 @@
 		{
 			$this->load->view('gelen_malzeme_gir');
 		}
-		public function form_kontrolu()
+		public function formKontrolu()
 		{
+			$this->load->library('form_validation');
+			$this->form_validation->set_rules('gelen_firma', 'Firma İsmi', 'trim|required|min_length[3]|max_length[30]|xss_clean');	
+			$this->form_validation->set_rules('urun_ismi', 'Ürün ismi', 'trim|required|min_length[3]|max_length[30]|xss_clean');
+			$this->form_validation->set_rules('urun_kodu', 'Ürün Kodu', 'trim|required|min_length[2]|max_length[30]|xss_clean');
+
+			if($this->form_validation->run() == FALSE)
+			{
+				$this->load->view('gelen_malzeme_gir');
+			} else
+				$this->load->view('gelen_malzeme_gir');
+			}
+
 
 		}
 	}
---
+++ application/views/gelen_malzeme_gir_basarili.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Başlık bu</title>
+	</head>
+	
+	<body>
+		<h1>Gelen Malzeme Ekleme Başarılı!</h1>
+		<h4>Girilen Malzeme bilgileri: </h4>
+		
+		
+	</body>
+
+</html>
