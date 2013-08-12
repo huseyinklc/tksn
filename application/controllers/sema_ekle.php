f04e92743be8b069d27519152b727780108fe3fb
huseyiklc <huseyinklc@yahoo.co.uk>
2013-08-12 16:45:09 +0300

    sema_ekle sayfasının controller ve view dosyalari olusturuldu, upload kismi duzeltilecek

---
+++ application/controllers/sema_ekle.php
@@ -0,0 +1,25 @@
+<?php
+
+	class Sema_Ekle extends CI_Controller
+	{
+		public function index()
+		{
+			$veri['upload_sayisi'] = 1;
+			$veri['dosya_yukleme_hatasi'] = '';
+			$this->load->view('sema_ekle', $veri);
+		}
+		public function form_kontrolu()
+		{
+		
+		$config['upload_path'] = '../uploads/';
+		$config['allowed_types'] = 'gif|jpeg|jpg|png';
+		$config['max_size'] = '2000';
+		$config['max_width'] = '1024';
+		$config['max_height'] = '768';
+
+			$this->load->library('upload', $config);	
+			$this->upload->do_upload();
+			print_r($this->upload->data());
+			print_r($this->upload->display_errors());
+		}
+	}
--- application/views/sema_ekle.php
+++ application/views/sema_ekle.php
@@ -1,13 +1,30 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>Başlık bu</title>
+		<title>Şema Ekle</title>
 	</head>
 	
 	<body>
-		<h1>Root sayfası</h1>
-		<p>Deneme</p>
-		<a href="giris/cikis">Çıkış için tıklayınız...</a>
+
+	<? echo $dosya_yukleme_hatasi; ?>
+	<?php $this->load->helper('form'); ?>
+	<?php echo form_open_multipart('sema_ekle/form_kontrolu'); ?>
+
+	<p>
+	<label>Lütfen Eklemek istediğiniz şema sayısını şeçiniz: </label>
+
+	<?php 
+		// $proje_sayisi = array('name'=>'proje_sayisi', 'id'=>'proje_sayisi', 'maxlength'=>'1');
+		// echo form_input($proje_sayisi);
+
+		
+
+		echo form_upload('sema');
+		echo form_submit('yukle', 'yukle');
+		echo form_close();
+	?>
+
+	</p>
 	</body>
 
 </html>
