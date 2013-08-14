15d03d5126d7aea6f18c35ee5f79953570ba8eea
huseyiklc <huseyinklc@yahoo.co.uk>
2013-08-14 14:31:22 +0300

    upload sorunu çözüldü ve textarea eklendi

--- application/controllers/proje_ekle.php
+++ application/controllers/proje_ekle.php
@@ -2,10 +2,16 @@
 
 	class Proje_ekle extends CI_Controller
 	{
+		public function __construct()
+		{
+			parent::__construct();
+			$this->load->helper('form');
+			$this->load->library('form_validation');
+		}
+
 		public function index()
 		{
 			$veri['hata'] = '';
-			$this->load->helper(array('form'));
 			$this->load->view('proje_ekle', $veri);
 		
 		}
@@ -14,21 +20,19 @@
 		{
 
 
-			$this->load->library('form_validation');
+			
 			$this->form_validation->set_rules('proje_ismi', 'Proje ismi', 'required|max_length[15]|min_length[4]|alpha_numeric|xss_clean');
-
-
 			$proje_ismi = $this->input->post('proje_ismi');
 			
 			$dosya_ayari['upload_path'] = './uploads/';
-			$dosya_ayari['allowed_types'] = 'gif|jpg|png';
+			$dosya_ayari['allowed_types'] = 'gif|jpg|png|jpeg';
 			$dosya_ayari['max_size']	= '2048';
 			$dosya_ayari['max_width']  = '1024';
 			$dosya_ayari['max_height']  = '768';
 
 			$this->load->library('upload', $dosya_ayari);
 
-			if ( ! $this->upload->do_upload() || ($this->form_validation->run() == FALSE) )
+			if ( !$this->upload->do_upload() || ($this->form_validation->run() == FALSE) )
 			{
 
 				$error = array('error' => $this->upload->display_errors());
--- application/views/proje_ekle.php
+++ application/views/proje_ekle.php
@@ -16,13 +16,18 @@
 	echo form_input($isim_formu_ozellikleri);
  ?>
 </p>
+</p>
 <?php 
 	$upload_form_ozellikleri = array('type'=>'file', 'name'=>'userfile');
 	echo	form_upload($upload_form_ozellikleri);	
 ?>
-
-
-
+</p>
+<p>
+<?php 
+	$kisa_proje_tanimi = array('name'=>'kisa_proje_tanimi', 'id'=>'kisa_proje_tanimi', 'cols'=>'100', 'rows'=>30);
+	echo form_textarea($kisa_proje_tanimi);
+?>
+</p>
 <br /><br />
 
 <input type="submit" value="upload" />
