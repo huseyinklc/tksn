62fa27766471d034c355c498c5738fced7fbf434
huseyiklc <huseyinklc@yahoo.co.uk>
2013-08-23 16:12:19 +0300

    Dokuman başarılı eklendi sayfası basit bir şekilde yapıldı

---
+++ application/views/arge/dokuman/dokuman_ekle_basarili.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Döküman Başarılı Bir Şekilde Eklendi</title>
+	</head>
+	
+	<body>
+		<h1>Döküman Başarılı Bir Şekilde Eklendi!!!</h1>
+	
+	</body>
+
+</html>
