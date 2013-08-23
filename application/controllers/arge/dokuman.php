76e2eeb64b126b99b4debc5bcd859afc32ee7d7f
huseyiklc <huseyinklc@yahoo.co.uk>
2013-08-23 16:46:31 +0300

    dokuman ekleme ve database yamak için gerekli controller fonksiyonları yazıldı

--- application/controllers/arge/dokuman.php
+++ application/controllers/arge/dokuman.php
@@ -20,4 +20,33 @@
 			$veri['dosya_yukleme_hatasi'] = '';
 			$this->load->view('arge/dokuman/dokuman_ekle', $veri );
 		}
+
+		public function form_kontrolu()
+		{
+			$ayar = $this->dokuman_model->dokuman_upload_ozellikleri();
+
+			$this->load->library('upload', $ayar);	
+			
+			if(! $this->upload->do_upload())
+			{
+ 				// hata durumu 
+			} else {
+
+				// Eğer herşey yolunda gittiyse
+				// upload bilgileri ve dokuman ismini array içine atıyoruz
+
+				$database_yazilacak_bilgiler['upload_bilgileri'] = $this->upload->data();
+				$database_yazilacak_bilgiler['dokuman_ismi'] = $database_yazilacak_bilgiler['upload_bilgileri']['file_name'];
+
+				// formdaki verileri database e yazmayı deniyoruz..
+				if($this->dokuman_model->sema_database_ekle($database_yazilacak_bilgiler)) {
+						// eğer form bilgileri database e aktarma işlemi başarılı ise, dokuman_ekle_başarılı sayfasını girilen bilgiler
+						// ile ekrana yazdırıyoruz 
+						$this->load->view('arge/dokuman/dokuman_ekle_basarili', $database_yazilacak_bilgiler);
+				} else {
+					// Buraya internal server error sayfası gelecek!!!
+					echo 'internal server error';
+				}
+			}
+		}
 	}
