b4b9c8cb98aef22157d08c2a86b77ba1950e12c7
huseyiklc <huseyinklc@yahoo.co.uk>
2013-08-23 16:21:37 +0300

    dokuman ekle view sayfası yaratıldı

--- application/views/arge/dokuman/dokuman_ekle.php
+++ application/views/arge/dokuman/dokuman_ekle.php
@@ -5,7 +5,20 @@
 	</head>
 	
 	<body>
-		
+		<?php echo $dosya_yukleme_hatasi; ?>
+		<?php $this->load->helper('form'); ?>
+		<?php echo form_open_multipart('arge/dokuman/form_kontrolu'); ?>
+
+	<p>
+	<label>Lütfen Eklemek istediğiniz dökümanı seçiniz: </label>
+
+	<?php 
+		echo form_upload('Dokuman');
+		echo form_submit('yukle', 'yukle');
+		echo form_close();
+	?>
+
+	</p>
 	
 	</body>
 
