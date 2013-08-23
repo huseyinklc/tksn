82e97ca907616ebc8d6e815b67c70ed5f53d5b95
huseyiklc <huseyinklc@yahoo.co.uk>
2013-08-23 16:47:15 +0300

    eklenmemiş document controller dosyası

---
+++ application/views/arge/dokuman/dokumanlar.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Dökümanlar</title>
+	</head>
+	
+	<body>
+		<h1>Dökümanlar</h1>
+
+		<a href = "dokuman/dokuman_ekle" >Döküman Eklemek için tıklayınız..</a>
+	
+	</body>
+
+</html>
