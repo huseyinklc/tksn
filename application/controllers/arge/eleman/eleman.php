a9d777901a1dd05386c930c3b1126173706096e9
huseyinklc <hsyinklc@yahoo.co.uk>
2013-08-25 14:22:38 +0300

    eleman ve eleman_ekle controllerları yaratıldı

---
+++ application/controllers/arge/eleman/eleman.php
@@ -0,0 +1,17 @@
+<?php 
+	class Eleman extends CI_extends
+	{
+		public function __construct()
+		{
+			// Framework parent constuructor çağırıldı..
+			parent::__construct();
+
+			
+		}
+
+		public function index(){
+
+		}
+	}
+/* End of the file: eleman.php */
+/* Location: ./application/controllers/arge/eleman/ */
---
+++ application/controllers/arge/eleman/eleman_ekle.php
@@ -0,0 +1,17 @@
+<?php 
+	class Eleman_Ekle extends CI_extends
+	{
+		public function __construct()
+		{
+			// Framework parent constuructor çağırıldı..
+			parent::__construct();
+
+			
+		}
+
+		public function index(){
+
+		}
+	}
+/* End of the file: eleman_ekle.php */
+/* Location: ./application/controllers/arge/eleman/ */
