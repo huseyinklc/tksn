4664e6b29756170c5b8e7e87a9110a6e8d868fd5
huseyinklc <hsyinklc@yahoo.co.uk>
2013-08-25 12:59:57 +0300

    sema goster session kontrolu ile sadece argeden erişilecek şekilde değiştirildi

--- application/controllers/arge/sema_goster.php
+++ application/controllers/arge/sema_goster.php
@@ -7,11 +7,23 @@
 
 		public function __construct()
 		{
+			// Framework constructor 
 			parent::__construct();
+
+			// Şemaları databaseden çekebilmek için sema_model'i yükledik
 			$this->load->model('arge/sema_model');
+
+			// databasedeki bulunan şema bilgilerini class sema_bilgilerine aktardık
 			$this->sema_bilgileri = $this->sema_model->sema_goster();
+
+			// Sadece arge kullanıcısının girdiği session ile kontrol edildi
+			if( $this->session->userdata('uyelik_turu') != 2) {
+			 	redirect('giris', 'refresh');
+			}
 		}
 		public function index(){
+
+			// Şema bilgileri ile sema_goster sayfasını yükledik
 			$veri['sema_bilgileri'] = $this->sema_bilgileri;
 			$this->load->view('arge/sema/sema_goster', $veri);
 		}
