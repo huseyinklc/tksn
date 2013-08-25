05974645bf69c8eb35be50531c95249a4ed5de70
huseyinklc <hsyinklc@yahoo.co.uk>
2013-08-25 12:41:25 +0300

    dokumanlar sayfası session ile bir tek argeden girilebilecek hale getirildi. Dökümantasyon yapıldı

--- application/controllers/arge/dokuman.php
+++ application/controllers/arge/dokuman.php
@@ -4,29 +4,51 @@
 	{
 		public function __construct()
 		{
+			// Framework ana construction
 			parent::__construct();
+
+			// Database işlemleri için
 			$this->load->model('arge/dokuman_model');
+
+			// Form Kontrolü için
 			$this->load->helper('form');
+
+			// Arge dışından kullanıcılar erişemesin diye..
+			if( $this->session->userdata('uyelik_turu') != 2) {
+			 	redirect('giris', 'refresh');
+			 }
 		}
 
 		public function index()
 		{
+			// Eğer sayfa ilk defa yüklenecek ise hata boş string olarak yazılır böylece
+			// hiçbir hata göserilmez
 			$veri['dosya_yukleme_hatasi'] = '';
 			$this->load->view('arge/dokuman/dokumanlar');
 		}
-
+		
+		/**
+		 * Döküman eklenme istediğinde çağırılacak sayfa yüklenir..
+		 * * @return [type] [description]
+		 */
 		public function dokuman_ekle()
 		{
 			$veri['dosya_yukleme_hatasi'] = '';
 			$this->load->view('arge/dokuman/dokuman_ekle', $veri );
 		}
-
+		/**
+		 * Eklenen dökümanda herhangi bir hata olup olmadığı kontrol edilir..
+		 * @return [type] [description]
+		 */
 		public function form_kontrolu()
 		{
+			// döküman upload özellikleri dokuman_model sayafasından çekilir..
 			$ayar = $this->dokuman_model->dokuman_upload_ozellikleri();
 
+			// framework upload sayfası yüklendi
 			$this->load->library('upload', $ayar);	
 			
+			// eğer upload durumunda bir sıkıntı varsa
 			if(! $this->upload->do_upload())
 			{
  				// hata durumu 
@@ -51,3 +73,5 @@
 			}
 		}
 	}
+/* End of file: dokuman.php */
+/* Location: ./application/controllers/arge/ */
