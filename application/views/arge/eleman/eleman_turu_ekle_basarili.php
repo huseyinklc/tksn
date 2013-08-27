b9611aaceebac3358a40715cdda5b005dbc1cec7
huseyiklc <huseyinklc@yahoo.co.uk>
2013-08-27 12:05:46 +0300

    eleman_turu_ekle_basarili sayfasi yapildi

---
+++ application/views/arge/eleman/eleman_turu_ekle_basarili.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Eleman Türü Ekleme Başarılı</title>
+	</head>	
+	<body>
+		<h1>Eleman Türü Ekleme Başarılı</h1>
+		<h3>Eklenen Eleman Türü Bilgileri: </h3>
+		<Strong>Eleman Türü: </Strong><?php echo $eleman_turu; ?>
+	</body>
+</html>
+<!-- End of the File:  eleman_turu_ekle_basarili.php -->
+<!-- Lacation: ./applicatipn/views/arge/eleman/ -->
