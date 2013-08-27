692267f22f3918dcde2f4cc089bfc15170c53597
huseyiklc <huseyinklc@yahoo.co.uk>
2013-08-27 16:16:04 +0300

    Database başlıkları ile tablo oluşturuldu

--- application/views/arge/eleman/eleman_goster.php
+++ application/views/arge/eleman/eleman_goster.php
@@ -6,7 +6,22 @@
 	
 	<body>
 		<h1>Eleman Göster</h1>
-
+	<table >
+	    <thead WIDTH="100%">
+	        <tr><th>Firma İsmi</th><th>Eleman Kodu</th>
+	        <th>Eleman Türü</th><th>Kılıf</th>
+	        <th>Adet</th><th>Numune</th>
+	        <th>Detay</th><th>Değiştir</th>
+	        <th>Adet</th></tr>
+	    </thead>
+	    <tbody WIDTH="100%">
+	        <tr><td>Hücre 1-1</td><td>Hücre 1-2</td>
+	        <td>Hücre 2-1</td><td>Hücre 2-2</td>
+	        <td>Hücre 1-1</td><td>Hücre 1-1</td>
+	    	<td>Hücre 1-1</td><td>Hücre 1-1</td>
+			<td>Hücre 1-1</td></tr>
+	    </tbody>
+	</table>
 	
 		
 	</body>
